5c92e25bf0a5f529dd725c09d76e297ffb24a07e
Hess <hess90wang@gmail.com>
2026-09-01 09:24:08 +0900

    AgentState

--- agent-foundations/src/App.tsx
+++ agent-foundations/src/App.tsx
@@ -1,5 +1,28 @@
+import { useAgent } from "agents/react";
+import { useState } from "react";
+import { ChattingRoomAgent} from '../worker/index'
+import type {PingPongState} from '../worker/index'
+
+
+
 function App() {
-	return <></>;
+	const [isConnected, setIsConnected] = useState(false);
+	const[pingPongs, setPingPongs] = useState(0);
+	const agent = useAgent<ChattingRoomAgent, PingPongState>({
+		agent: 'AGENT',
+		onOpen: () => setIsConnected(true),
+		onStateUpdate: (state) => setPingPongs(state.pingPongCount),
+	});
+
+	if (!isConnected) return <h1>connecting...</h1>;
+
+	return (
+	<div>
+		<h1>Ping Pong Agent</h1>
+		<h3>Count: {pingPongs}</h3>
+		
+	</div>
+	);
 }
 
 export default App;
