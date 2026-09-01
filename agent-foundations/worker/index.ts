0ac8f3a093671c94b65fd7eacbffd5150ec4796e
Hess <hess90wang@gmail.com>
2026-09-01 17:22:20 +0900

    Storage and Broadcast

--- agent-foundations/worker/index.ts
+++ agent-foundations/worker/index.ts
@@ -1,4 +1,9 @@
-import { Agent, routeAgentRequest, type Connection, type WSMessage } from "agents";
+import {
+  Agent,
+  routeAgentRequest,
+  type Connection,
+  type WSMessage,
+} from "agents";
 
 export type ChattingRoomState = {
   currentlyOnline: number;
@@ -9,20 +14,52 @@ export class ChattingRoomAgent extends Agent<Env, ChattingRoomState> {
     currentlyOnline: 0,
   };
 
-  onConnect(){
-	this.setState({
-		currentlyOnline: this.state.currentlyOnline +1
-	})
+  onStart() {
+    void this.sql`
+	CREATE TABLE IF NOT EXISTS messages (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	nickname TEXT NOT NULL,
+	message TEXT NOT NULL,
+	created_at INTEGER NOT NULL)
+	`;
   }
 
-  onClose(){
-	this.setState({
-		currentlyOnline: this.state.currentlyOnline -1,
-	})
+  onStateChanged(
+    state: ChattingRoomState,
+    source: Connection | "server",
+  ): void {
+    console.log("new state", state);
+    console.log("who did it", source);
+  }
+
+  validateStateChange(
+    _nextState: ChattingRoomState,
+    source: Connection | "server",
+  ): void {
+    if (source !== "server") throw new Error("cant do this");
+  }
+
+  onConnect() {
+    this.setState({
+      currentlyOnline: this.state.currentlyOnline + 1,
+    });
+  }
+
+  onClose() {
+    this.setState({
+      currentlyOnline: this.state.currentlyOnline - 1,
+    });
   }
   onMessage(connection: Connection, message: WSMessage): void | Promise<void> {
-	console.log(message);
-	connection.send('back');
+	const messageObj = {
+		nickname: 'anon',
+		message: message.toString(),
+		created_at: Date.now(),
+	};
+	void this.sql`
+	INSERT INTO messages ( nickname, message, created_at) VALUES (${messageObj.nickname}, ${messageObj.message}, ${messageObj.created_at})`;
+
+	this.broadcast(JSON.stringify(messageObj), [connection.id])
   }
 }
 
