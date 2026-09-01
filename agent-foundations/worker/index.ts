10316a42a94a44774a812742e40d1cfc6874fd15
Hess <hess90wang@gmail.com>
2026-09-01 18:25:11 +0900

    Authentication

--- agent-foundations/worker/index.ts
+++ agent-foundations/worker/index.ts
@@ -1,7 +1,10 @@
 import {
   Agent,
+  callable,
+  getCurrentAgent,
   routeAgentRequest,
   type Connection,
+  type ConnectionContext,
   type WSMessage,
 } from "agents";
 
@@ -24,13 +27,13 @@ export class ChattingRoomAgent extends Agent<Env, ChattingRoomState> {
 	`;
   }
 
-  onStateChanged(
-    state: ChattingRoomState,
-    source: Connection | "server",
-  ): void {
-    console.log("new state", state);
-    console.log("who did it", source);
-  }
+//   onStateChanged(
+//     state: ChattingRoomState,
+//     source: Connection | "server",
+//   ): void {
+//     console.log("new state", state);
+//     console.log("who did it", source);
+//   }
 
   validateStateChange(
     _nextState: ChattingRoomState,
@@ -39,10 +42,13 @@ export class ChattingRoomAgent extends Agent<Env, ChattingRoomState> {
     if (source !== "server") throw new Error("cant do this");
   }
 
-  onConnect() {
-    this.setState({
-      currentlyOnline: this.state.currentlyOnline + 1,
-    });
+  onConnect(connection: Connection, ctx: ConnectionContext) {
+	const url = new URL(ctx.request.url);
+	const nickname = url.searchParams.get('nickname') ?? 'anon';
+
+	connection.setState({
+		nickname,
+	})
   }
 
   onClose() {
@@ -50,21 +56,30 @@ export class ChattingRoomAgent extends Agent<Env, ChattingRoomState> {
       currentlyOnline: this.state.currentlyOnline - 1,
     });
   }
-  onMessage(connection: Connection, message: WSMessage): void | Promise<void> {
+  onMessage(connection: Connection<{nickname: string}>, message: WSMessage): void | Promise<void> {
 	const messageObj = {
-		nickname: 'anon',
+		nickname: connection.state?.nickname ?? 'anon',
 		message: message.toString(),
 		created_at: Date.now(),
 	};
 	void this.sql`
 	INSERT INTO messages ( nickname, message, created_at) VALUES (${messageObj.nickname}, ${messageObj.message}, ${messageObj.created_at})`;
 
-	this.broadcast(JSON.stringify(messageObj), [connection.id])
+	// this.broadcast(JSON.stringify(messageObj), [connection.id])
+	this.broadcast(JSON.stringify(messageObj));
   }
+
+	@callable()
+	loadHistory(){
+		const {connection} = getCurrentAgent<ChattingRoomAgent>();
+		console.log(connection?.state,'loaded history');
+		return this.sql`SELECT * FROM message ORDER BY created_at ASC LIMIT 100`;
+	}
 }
 
 export default {
   async fetch(request, env) {
+	console.log(request.url)
     const agentResponse = await routeAgentRequest(request, env);
     if (agentResponse) return agentResponse;
     return new Response(null, { status: 404 });
