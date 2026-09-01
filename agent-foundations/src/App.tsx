fce14c5e86aa162696d090cc857d26b5a2953bc1
Hess <hess90wang@gmail.com>
2026-09-01 18:25:08 +0900

    Authentication

--- agent-foundations/src/App.tsx
+++ agent-foundations/src/App.tsx
@@ -2,31 +2,81 @@ import { useAgent } from "agents/react";
 import { useState } from "react";
 import type { ChattingRoomAgent, ChattingRoomState } from "../worker/index";
 
+type Message = {
+  id: number;
+  nickname: string;
+  message: string;
+  created_at: number;
+};
+
 function App() {
   const [isConnected, setIsConnected] = useState(false);
   const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [nickname, setNickname] = useState("");
+  const [ready, setReady] = useState(false);
+
   const agent = useAgent<ChattingRoomAgent, ChattingRoomState>({
     agent: "AGENT",
-    onOpen: () => setIsConnected(true),
-    onMessage: (event) => console.log(event),
+    query: {
+      nickname,
+    },
+    enabled: ready,
+    onOpen: async () => {
+      setIsConnected(true);
+      const history = (await agent.stub.loadHistory()) as Message[];
+      setMessages(history);
+    },
+    onMessage: (event) =>
+      setMessages((prev) => [...prev, JSON.parse(event.data)]),
   });
   const sendMessage = () => {
     agent.send(message);
     setMessage("");
   };
-  if (!isConnected) return <h1>connecting...</h1>;
-
+  const onConfirm = () => {
+    setReady(true);
+  };
+  if (!isConnected)
+    return (
+      <div>
+        <h1>who r u ?</h1>
+        <input
+          type="text"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+          placeholder="Type a nickname"
+          autoFocus
+        />
+        <button onClick={onConfirm}>confirm</button>
+      </div>
+    );
   return (
     <div>
-      <h1>Ping Pong Agent</h1>
-      <h3>Count: {agent?.state?.currentlyOnline}</h3>
+      <h1>Charring Room Agent</h1>
+      <h3>Onlindppl: {agent?.state?.currentlyOnline}</h3>
       <hr />
-			<form onSubmit={(e)=>{
-				e.preventDefault();
-				sendMessage();
-			}}>
-				<input type="text" value={message}  onChange={(e)=>setMessage(e.target.value)} placeholder="Type a message..." autoFocus/>
-			</form>
+      <ul>
+        {messages.map((message) => (
+          <li>
+            <strong>{message.nickname}</strong>:{message.message}
+          </li>
+        ))}
+      </ul>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage();
+        }}
+      >
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type a message..."
+          autoFocus
+        />
+      </form>
     </div>
   );
 }
