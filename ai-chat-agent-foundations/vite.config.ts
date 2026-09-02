6204f7602f9ca5df3040017c91395c3f316dd185
Hess <hess90wang@gmail.com>
2026-09-02 22:41:26 +0900

    lecture fin

--- ai-chat-agent-foundations/vite.config.ts
+++ ai-chat-agent-foundations/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 import agents from "agents/vite";
@@ -8,4 +8,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [agents(), react(), tailwindcss(), cloudflare()],
-})
+});
--- ai-chat-agent-foundations/worker/index.ts
+++ ai-chat-agent-foundations/worker/index.ts
@@ -1,10 +1,57 @@
 import { AIChatAgent } from "@cloudflare/ai-chat";
 import { routeAgentRequest } from "agents";
+import {
+  convertToModelMessages,
+  isLoopFinished,
+  streamText,
+  type StreamTextOnFinishCallback,
+  type ToolSet,
+  type UIMessage,
+} from "ai";
+import { createWorkersAI } from "workers-ai-provider";
+import { buyPlaneTicket, getLocation, getTickets, getWeather } from "./tools";
 
-export class PotatoChatAgent extends AIChatAgent<Env> {}
+export class PotatoChatAgent extends AIChatAgent<Env> {
+  async onChatMessage(
+    _onFinish: StreamTextOnFinishCallback<ToolSet>,
+    options?: { abortSignal?: AbortSignal },
+  ) {
+    const workersAi = createWorkersAI({
+      binding: this.env.AI,
+    });
+    const result = streamText({
+      model: workersAi("@cf/zai-org/glm-4.7-flash"),
+      messages: await convertToModelMessages(this.messages),
+      tools: {
+        getWeather,
+        getLocation,
+        getTickets,
+        buyPlaneTicket,
+      },
+      abortSignal: options?.abortSignal,
+      stopWhen: isLoopFinished(),
+    });
+    return result.toUIMessageStreamResponse();
+  }
+
+  sanitizeMessageForPersistence(message: UIMessage): UIMessage {
+    return {
+      ...message,
+      parts: message.parts.map((part) => {
+        if (part.type === "text") {
+          return {
+            ...part,
+            text: part.text.replace("food", "❌ stop eating u fat ❌"),
+          };
+        }
+        return part;
+      }),
+    };
+  }
+}
 
 export default {
-  async fetch(request, env): Promise<Response> {
+  async fetch(request: Request, env: Env): Promise<Response> {
     return (
       (await routeAgentRequest(request, env)) ??
       new Response(null, { status: 404 })
